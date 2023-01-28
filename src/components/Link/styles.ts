3997dc96be9da4a83b627f8772609059bc0a68f4
Angelo Filho <angelo.filho06@hotmail.com>
2023-01-28 19:45:42 -0300

    feat: add hover effect to link component

--- src/components/Link/styles.ts
+++ src/components/Link/styles.ts
@@ -6,6 +6,7 @@ export const NavLink = styled(RouterLink)`
 `;
 
 export const LinkChildren = styled.span<{ active: boolean }>`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -15,4 +16,23 @@ export const LinkChildren = styled.span<{ active: boolean }>`
     props.active
       ? props.theme.colors.mainGreen
       : props.theme.colors.titleColor};
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: -8px;
+
+    width: 130%;
+    height: 2px;
+
+    background-color: ${(props) => props.theme.colors.mainGreen};
+
+    transform: scaleX(0);
+
+    transition: transform 0.2s;
+  }
+
+  &:hover::before {
+    transform: scaleX(1);
+  }
 `;
